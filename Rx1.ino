18809765a50e65562671890e6f08da23096e5865
mctomss <tomas.montanez-p@mail.escuelaing.edu.co>
2025-03-10 00:09:25 -0500

    Agregar Archivos de proyecto
    Códigos de Tx y Rx para las ESP32 de transmisión y recepción.

---
+++ Rx1.ino
@@ -0,0 +1,233 @@
+#include <LoRa.h>
+#include <SPI.h>
+#include <WiFi.h>
+#include <Arduino.h>
+#include <ThingerESP32.h>
+#include <driver/i2s.h>
+#include <math.h>
+
+#define THINGER_SERIAL_DEBUG
+#define RXD2 16
+#define TXD2 17
+#define USERNAME "McTomSs"
+#define DEVICE_ID "ESP32_METX"
+#define DEVICE_CREDENTIAL "tqmSLAKcR+9kq5vl"
+#define SSID "moto g84 5G_6199"
+#define SSID_PASSWORD "tomasito2003"
+
+ThingerESP32 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
+
+#define ss 5
+#define rst 14
+#define dio0 2
+
+float h1, t1, h2, t2, w, v, db;  // Variables globales
+int c, contador, prx;            // Variables globales
+float voltage_ref = 1.0;         // Referencia de 1V
+
+void printWiFiStatus() {
+  switch (WiFi.status()) {
+    case WL_IDLE_STATUS:
+      Serial.println("WiFi está inactivo, esperando conexión.");
+      break;
+    case WL_NO_SSID_AVAIL:
+      Serial.println("SSID no disponible. Verifica el nombre de la red.");
+      break;
+    case WL_CONNECT_FAILED:
+      Serial.println("Error de conexión. Verifica la contraseña.");
+      break;
+    case WL_DISCONNECTED:
+      Serial.println("WiFi desconectado. Intentando reconectar...");
+      break;
+    case WL_CONNECTED:
+      Serial.println("WiFi conectado correctamente.");
+      Serial.print("Dirección IP: ");
+      Serial.println(WiFi.localIP());
+      break;
+    default:
+      Serial.println("Estado desconocido.");
+  }
+}
+
+void Iniciar() {
+  pinMode(2, OUTPUT);
+
+  Serial.println("Conectando a WiFi...");
+  WiFi.begin(SSID, SSID_PASSWORD);
+
+  unsigned long startAttemptTime = millis();
+  const unsigned long timeout = 10000;  // Tiempo límite: 10 segundos
+
+  while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < timeout) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("\nConexión WiFi establecida!");
+    Serial.print("Dirección IP: ");
+    Serial.println(WiFi.localIP());
+  } else {
+    Serial.println("\nError: No se pudo conectar al WiFi.");
+    printWiFiStatus();
+  }
+
+  // Configuración de Thinger.io
+  thing.add_wifi(SSID, SSID_PASSWORD);
+  thing["GPIO_2"] << digitalPin(2);
+}
+
+void setupLoRa() {
+  Serial.println("Configurando LoRa...");
+  LoRa.setPins(ss, rst, dio0);  // Configurar módulo LoRa
+
+  while (!LoRa.begin(433E6)) {  // Configuración según la región
+    Serial.println("Intentando configurar LoRa...");
+    delay(500);
+  }
+  LoRa.setSyncWord(0xA5);
+  Serial.println("LoRa configurado correctamente!");
+}
+
+void setup() {
+  Serial.begin(115200);  // Velocidad de comunicación
+  Iniciar();
+  setupLoRa();
+
+  // Configuración de recursos en Thinger.io
+  thing["Humedad i"] >> [](pson &out) {
+    out = h1;
+    Serial.println("Se envió humedad i: " + String(h1));
+  };
+  thing["Temperatura i"] >> [](pson &out) {
+    out = t1;
+    Serial.println("Se envió temperatura i: " + String(t1));
+  };
+  thing["Humedad e"] >> [](pson &out) {
+    out = h2;
+    Serial.println("Se envió humedad e: " + String(h2));
+  };
+  thing["Temperatura e"] >> [](pson &out) {
+    out = t2;
+    Serial.println("Se envió temperatura e: " + String(t2));
+  };
+  thing["Peso"] >> [](pson &out) {
+    out = w;
+    Serial.println("Se envió peso: " + String(w));
+  };
+  thing["Potencia_Sonido"] >> [](pson &out) {
+    out = db;
+    Serial.println("Se envió potencia: " + String(db));
+  };
+  thing["Cantidad"] >> [](pson &out) {
+    out = c;
+    Serial.println("Se envió cantidad: " + String(c));
+  };
+  thing["RRSI"] >> [](pson &out) {
+    out = prx;
+    Serial.println("Se envió cantidad: " + String(prx));
+  };
+  thing["Datos"] >> [](pson &out) {
+    out = contador;
+    Serial.println("Se envió Total Datos Enviados: " + String(contador));
+  };
+
+
+  //VARIABLES PARA LA INTERFAZ DE USUARIO
+  // Porcentaje de miel
+  thing["miel_porcentaje"] >> [](pson &out) {
+    out = (w / 4.0) * 100;  // Convertir de 0-4 kg a 0-100%
+    Serial.println("Se envió miel_porcentaje: " + String((w / 4.0) * 100) + "%");
+  };
+
+
+  // Estado de la temperatura
+  thing["estado_temperatura"] >> [](pson &out) {
+    if (t1 < 10) {
+      out = "Muy Baja";
+    } else if (t1 >= 10 && t1 <= 25) {
+      out = "Baja";
+    } else if (t1 >= 26 && t1 <= 33) {
+      out = "Regular";
+    } else if (t1 >= 34 && t1 <= 36) {
+      out = "Ideal";
+    } else if (t1 >= 37 && t1 <= 40) {
+      out = "Alta";
+    } else {
+      out = "Muy Alta";
+    }
+    Serial.println("Se envió estado_temperatura: " + String(out));
+  };
+
+  // Estado de la humedad
+  thing["estado_humedad"] >> [](pson &out) {
+    if (h1 < 30) {
+      out = "Muy Baja";
+    } else if (h1 >= 30 && h1 <= 50) {
+      out = "Baja";
+    } else if (h1 >= 51 && h1 <= 60) {
+      out = "Regular";
+    } else if (h1 >= 61 && h1 <= 70) {
+      out = "Ideal";
+    } else if (h1 >= 71 && h1 <= 80) {
+      out = "Alta";
+    } else {
+      out = "Muy Alta";
+    }
+    Serial.println("Se envió estado_humedad: " + String(out));
+  };
+
+  thing["DATA_METX"] >> [](pson &out) {
+    out["Humedad_i"] = h1;
+    out["Humedad_e"] = h2;
+    out["Temperatura_i"] = t1;
+    out["Temperatura_e"] = t2;
+    out["Peso"] = w;
+    out["Voltaje"] = v;
+    out["Potencia_Sonido"] = db;
+    out["Cantidad"] = c;
+    out["RRSI"] = prx;
+    out["Datos"] = contador;
+  };
+}
+
+void loop() {
+  // Lógica de Thinger.io
+  thing.handle();
+
+  // Recepción de datos LoRa
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    Serial.println("Información recibida:");
+    String DatosRecibidos = "";
+    while (LoRa.available()) {
+      DatosRecibidos += (char)LoRa.read();
+    }
+
+    sscanf(DatosRecibidos.c_str(), "%f/%f/%f/%f/%d/%d/%f/%f", &h1, &t1, &w, &v, &c, &contador, &h2, &t2);
+    if (w < 0) {
+      w = 0;
+    }
+    db = 20 * log10(v / voltage_ref);
+    prx = LoRa.packetRssi();
+    // Mostrar datos recibidos
+    Serial.println("Humedad i: " + String(h1));
+    Serial.println("Humedad e: " + String(h2));
+    Serial.println("Temperatura i: " + String(t1));
+    Serial.println("Temperatura e: " + String(t2));
+    Serial.println("Peso: " + String(w));
+    Serial.println("Voltaje: " + String(v));
+    Serial.println("Abejas: " + String(c));
+    Serial.println("Contador: " + String(contador));
+    Serial.print("La potencia de recepción es ");
+    Serial.print(LoRa.packetRssi());
+    Serial.println(" dB.");
+  }
+
+  // Mostrar estado WiFi cada 10 segundos
+  static unsigned long lastCheck = 0;
+  if (millis() - lastCheck >= 10000) {
+    lastCheck = millis();
+    printWiFiStatus();
+  }
+}
---
+++ Tx1.ino
@@ -0,0 +1,161 @@
+#include <DHT.h>
+#include <DHT_U.h>
+#include <LoRa.h>
+#include <SPI.h>
+#include "HX711.h"
+#include <driver/i2s.h>
+
+// PINES y constantes
+const int LOADCELL_DOUT_PIN = 16;
+const int LOADCELL_SCK_PIN = 15;
+#define DHTPIN 4 
+#define DHTPIN2 21
+#define DHTTYPE DHT22 
+#define DHTTYPE2 DHT11
+#define ss 5
+#define rst 14
+#define dio0 2
+#define I2S_WS 25
+#define I2S_SD 33
+#define I2S_SCK 32
+#define I2S_PORT I2S_NUM_0
+
+#define bufferLen 64
+int16_t sBuffer[bufferLen];
+#define MIN_BEES 0
+#define MAX_BEES 1000
+
+int contador = 0;
+DHT dht1(DHTPIN, DHTTYPE);
+DHT dht2(DHTPIN2, DHTTYPE2);
+HX711 scale;
+
+unsigned long lastTime = 0;
+const unsigned long interval = 1000; // Intervalo en milisegundos
+
+void i2s_install() {
+    const i2s_config_t i2s_config = {
+        .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX),
+        .sample_rate = 44100,
+        .bits_per_sample = i2s_bits_per_sample_t(16),
+        .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+        .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_I2S),
+        .intr_alloc_flags = 0,
+        .dma_buf_count = 8,
+        .dma_buf_len = bufferLen,
+        .use_apll = false
+    };
+
+    i2s_driver_install(I2S_PORT, &i2s_config, 0, NULL);
+}
+
+void i2s_setpin() {
+    const i2s_pin_config_t pin_config = {
+        .bck_io_num = I2S_SCK,
+        .ws_io_num = I2S_WS,
+        .data_out_num = -1,
+        .data_in_num = I2S_SD
+    };
+
+    i2s_set_pin(I2S_PORT, &pin_config);
+}
+
+// Configuración de LoRa
+void setupLoRa() {
+    Serial.println("Configurando LoRa...");
+    LoRa.setPins(ss, rst, dio0);
+
+    while (!LoRa.begin(433E6)) { // Frecuencia según región
+        Serial.println("Intentando conectar a LoRa...");
+        delay(500);
+    }
+    LoRa.setSyncWord(0xA5);
+    Serial.println("LoRa configurado correctamente!");
+}
+
+// Leer sensores
+// Leer datos de micrófono
+void LeerSensores() {
+    delay(2000); // Espera para el sensor DHT
+    float h1 = dht1.readHumidity();
+    float t1 = dht1.readTemperature();
+    float h2 = dht2.readHumidity();
+    float t2 = dht2.readTemperature();
+
+    if (isnan(h1) || isnan(t1)) {
+        Serial.println("Error leyendo del sensor DHT22");
+        return;
+    }
+    if (isnan(h2) || isnan(t2)) {
+      Serial.println("Error leyendo del sensor DHT11");
+      return;
+    }
+    // Leer peso de la báscula
+    float weight = scale.get_units(10);
+
+    // Procesar datos del micrófono
+    float voltageRMS = 0;
+    int estimatedBees = 0;
+    float mayor_dato = 0; // Nuevo: Almacena el dato máximo absoluto
+
+    size_t bytesIn = 0;
+    esp_err_t result = i2s_read(I2S_PORT, &sBuffer, bufferLen * sizeof(int16_t), &bytesIn, portMAX_DELAY);
+    if (result == ESP_OK) {
+        int16_t samples_read = bytesIn / sizeof(int16_t);
+        if (samples_read > 0) {
+            for (int16_t i = 0; i < samples_read; ++i) {
+                float sample = abs(sBuffer[i]); // Trabajamos con valores absolutos
+                if (sample > mayor_dato) {
+                    mayor_dato = sample; // Actualizar el dato máximo
+                }
+            }
+
+            // Calcular voltaje RMS utilizando el dato máximo
+            voltageRMS = (mayor_dato / 32767.0) * 3.3;
+
+            // Mapear el voltaje al rango de abejas
+            estimatedBees = map(voltageRMS * 1000, 50, 3300, MIN_BEES, MAX_BEES);
+            if (estimatedBees < MIN_BEES) estimatedBees = MIN_BEES;
+            if (estimatedBees > MAX_BEES) estimatedBees = MAX_BEES;
+        }
+    }
+
+    // Enviar datos
+    enviarDatos(h1, t1, weight, voltageRMS, estimatedBees, h2, t2);
+}
+
+
+// Enviar datos
+void enviarDatos(float h1, float t1, float w, float vRMS, int bees, float h2, float t2) {
+    char sensores[100]; // Asegurar suficiente espacio
+    snprintf(sensores, sizeof(sensores), "%.2f/%.2f/%.2f/%.3f/%d/%d/%f/%f", h1, t1, w, vRMS, bees, contador, h2, t2);
+
+    LoRa.beginPacket();
+    LoRa.print(sensores);
+    LoRa.endPacket();
+    Serial.println(sensores);
+    contador++;
+}
+
+// INICIO PROGRAMA
+void setup() {
+    Serial.begin(115200);
+    Serial.println("Iniciando sistema...");
+    dht1.begin();
+    dht2.begin();
+    scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+    scale.set_scale(421);
+    scale.tare();
+    setupLoRa();
+    i2s_install();
+    i2s_setpin();
+    i2s_start(I2S_PORT);
+}
+
+void loop() {
+    if (millis() - lastTime >= interval) {
+        lastTime = millis();
+        LeerSensores();
+    }
+}
+
